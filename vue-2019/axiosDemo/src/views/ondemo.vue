cdcc9401f570f51b4fcebb3b4939ac6dfba400c5
wanghxing <1079563056@qq.com>
2019-08-22 18:24:37 +0800

    $on

---
+++ vue-2019/axiosDemo/src/views/ondemo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="onbox">
+    <oncomp-child-a></oncomp-child-a>
+    <br>
+    <oncomp-child-b></oncomp-child-b>
+  </div>
+</template>
+<script>
+  import childA from '../components/oncomp-child-a.vue'
+  import childB from '../components/oncomp-child-b.vue'
+  export default {
+     name:'on-demo',
+     data(){
+       return {
+         msg:''
+       }
+     },
+     components:{
+       'oncomp-child-a':childA,
+       'oncomp-child-b':childB
+     },
+     methods:{
+       
+     }
+  }
+</script>
