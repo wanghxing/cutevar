1bf48d9a5f1a52df960c187c390dd24b67410a9e
wanghxing <1079563056@qq.com>
2019-10-24 14:27:05 +0800

    computed更正 20191024

--- vue-2019/axiosDemo/src/components/Mutations.vue
+++ vue-2019/axiosDemo/src/components/Mutations.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h4>{{msg}}</h4>
-    <hr>
+    <hr />
     <h5>{{$store.state.count}}</h5>
     <!-- 调用mutations中的方法。 -->
     <div>
@@ -10,39 +10,34 @@
       <!-- <button @click="$store.commit('reduce')">-</button> -->
       <button @click="$store.commit('reduceBy', 10)">-10</button>
     </div>
-    
+
     <!-- <router-link to="/vuex-getters">vuex 04 getters计算过滤操作</router-link> -->
   </div>
 </template>
 
 <script>
-  import store from "@/vuex/store"
-  import {mapState, mapMutations} from "vuex"
-  export default {
-    name:'Mutations',
-    data() {
-      return {
-        msg: '03 Mutations修改状态'
-      }
-    },
-    store,
-    conputed: {
-      //...对象展开符的扩展 ...mapstate就是将mapstate对象混入到computed对象中
-      ...mapState([ 
-        'count'
-      ])
-    },
-    methods: {
-      ...mapMutations([
-        'add', 'incrementBy','reduce', 'reduceBy'
-      ])
-    }
+import store from "@/vuex/store";
+import { mapState, mapMutations } from "vuex";
+export default {
+  name: "Mutations",
+  data() {
+    return {
+      msg: "03 Mutations修改状态"
+    };
+  },
+  store,
+  computed: {
+    //...对象展开符的扩展 ...mapstate就是将mapstate对象混入到computed对象中
+    ...mapState(["count"])
+  },
+  methods: {
+    ...mapMutations(["add", "incrementBy", "reduce", "reduceBy"])
   }
+};
 </script>
 
 <style lang="scss" scoped>
 .container {
   width: 100%;
 }
-
 </style>
